4172bf74d69c496fffa1c8f65945812e6e23a1c9
dioptre <andrew@expedit.com.au>
2014-01-21 02:27:09 +0000

    init license mod

---
+++ Route.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Orchard.Mvc.Routes;
+
+namespace EXPEDIT.License
+{
+    public class Routes : IRouteProvider
+    {
+        public void GetRoutes(ICollection<RouteDescriptor> routes)
+        {
+            foreach (var routeDescriptor in GetRoutes())
+                routes.Add(routeDescriptor);
+        }
+
+        public IEnumerable<RouteDescriptor> GetRoutes()
+        {
+            return new[] {
+                new RouteDescriptor {
+                    Priority = 5,
+                    Route = new Route(
+                        "License/{controller}/{action}/{id}",
+                        new RouteValueDictionary {
+                            {"area", "EXPEDIT.License"},
+                            {"controller", "User"},
+                            {"action", "Index"}
+                        },
+                        new RouteValueDictionary {
+                            {"area", "EXPEDIT.License"},
+                            {"controller", "User"}
+                        },
+                        new RouteValueDictionary {
+                            {"area", "EXPEDIT.License"}
+                        },
+                        new MvcRouteHandler())
+                },
+                 new RouteDescriptor {
+                    Priority = 5,
+                    Route = new Route(
+                        "License/{action}/{id}/{name}/{contactid}",
+                        new RouteValueDictionary {
+                            {"area", "EXPEDIT.License"},
+                            {"controller", "User"}                            
+                        },
+                        new RouteValueDictionary {
+                            {"area", "EXPEDIT.License"},
+                            {"controller", "User"},                          
+                        },
+                        new RouteValueDictionary {
+                            {"area", "EXPEDIT.License"},
+                            {"controller", "User"}
+                        },
+                        new MvcRouteHandler())
+                },
+                 new RouteDescriptor {
+                    Priority = 5,
+                    Route = new Route(
+                        "License/{action}/{id}/{name}",
+                        new RouteValueDictionary {
+                            {"area", "EXPEDIT.License"},
+                            {"controller", "User"}                            
+                        },
+                        new RouteValueDictionary {
+                            {"area", "EXPEDIT.License"},
+                            {"controller", "User"},                          
+                        },
+                        new RouteValueDictionary {
+                            {"area", "EXPEDIT.License"},
+                            {"controller", "User"}
+                        },
+                        new MvcRouteHandler())
+                },
+                 new RouteDescriptor {
+                    Priority = 5,
+                    Route = new Route(
+                        "License/{action}/{id}",
+                        new RouteValueDictionary {
+                            {"area", "EXPEDIT.License"},
+                            {"controller", "User"}                            
+                        },
+                        new RouteValueDictionary {
+                            {"area", "EXPEDIT.License"},
+                            {"controller", "User"},                          
+                        },
+                        new RouteValueDictionary {
+                            {"area", "EXPEDIT.License"},
+                            {"controller", "User"}
+                        },
+                        new MvcRouteHandler())
+                },
+                new RouteDescriptor {
+                        Priority = 5,
+                        Route = new Route(
+                            "License/search",
+                            new RouteValueDictionary {
+                                {"area", "EXPEDIT.License"},
+                                {"controller", "User"},
+                                {"action", "search"}
+                            },
+                            null,
+                            new RouteValueDictionary {
+                                {"area", "EXPEDIT.License"}
+                            },
+                            new MvcRouteHandler())
+                }
+
+            };
+        }
+    }
+}
